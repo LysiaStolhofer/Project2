33579946d1166217dc4507716501dd855d38d178
Andrea <andreakwilliams1@gmail.com>
2019-11-21 21:50:44 -0500

    routes updated

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,25 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS petservices_db;
+CREATE DATABASE petservices_db;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+USE petservices_db;
+
+CREATE TABLE services (
+	item_id INTEGER(10) NOT NULL AUTO_INCREMENT,
+	  grooming VARCHAR(100) NOT NULL,
+    daycare VARCHAR(100) NOT NULL,
+    boarding VARCHAR(100) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    PRIMARY KEY (item_id)
+);
+
+CREATE TABLE clients (
+	client_id INTEGER(10) NOT NULL AUTO_INCREMENT,
+    pet_name VARCHAR(100) NOT NULL,
+    client_first_name VARCHAR(100) NOT NULL,
+    client_last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phonenumber INTEGER(10),
+);
+
+-- Insert mock services into database--
+INSERT INTO services (
