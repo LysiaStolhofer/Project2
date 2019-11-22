0bfb10cd9dd38711afbad878fa542aeaa0d6ff44
Andrea <andreakwilliams1@gmail.com>
2019-11-21 21:36:34 -0500

    route updated

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,30 +1,54 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
+  // Get all data
   app.get("/api/examples", async (req, res) => {
     try {
-      const data = await db.Example.findAll({});
+      const data = await db.PrimpnPlay_db.findAll({});
       res.json(data);
     } catch (error) {
       res.status(400).json({ error: { name: error.name, msg: error.message } });
     }
   });
 
-  // Create a new example
-  app.post("/api/examples", async (req, res) => {
+    // Get all client profiles
+    app.get("/api/clients", async (req, res) => {
+      try {
+        const data = await db.PrimpnPlay_db.findAll({});
+        res.json(data);
+      } catch (error) {
+        res.status(400).json({ error: { name: error.name, msg: error.message } });
+      }
+    });
+
+      // Get Pet Profile
+  app.get("/api/client/:id", async (req, res) => {
     try {
-      const result = await db.Example.create(req.body);
+      const data = await db.PrimpnPlay_db.findAll({
+        where: {
+          Id: id
+        }
+      });
+      res.json(data);
+    } catch (error) {
+      res.status(400).json({ error: { name: error.name, msg: error.message } });
+    }
+  });
+
+  // Create a new profile
+  app.post("/api/profile", async (req, res) => {
+    try {
+      const result = await db.PrimpnPlay_db.create(req.body);
       res.json(result);
     } catch (error) {
       res.status(400).json({ error: { name: error.name, msg: error.message } });
     }
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", async (req, res) => {
+  // Delete an Profile by id
+  app.delete("/api/profile/:id", async (req, res) => {
     try {
-      const result = await db.Example.destroy({ where: { id: req.params.id } });
+      const result = await db.PrimpnPlay_db.destroy({ where: { id: req.params.id } });
       const deletedRowCount = result;
       const status = deletedRowCount > 0 ? 200 : 404;
       res.status(status).json({ deletedRowCount });
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,10 +4,7 @@ module.exports = function(app) {
   // Load index page
   app.get("/", async (req, res) => {
     try {
-      const dbExamples = await db.Example.findAll({});
       res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
       });
     } catch (error) {
       res
@@ -15,6 +12,42 @@ module.exports = function(app) {
         .render("400", { error: { name: error.name, msg: error.message } });
     }
   });
+
+    // Load form page
+    app.get("/enroll", async (req, res) => {
+      try {
+
+        res.render("newform");
+      } catch (error) {
+        res
+          .status(400)
+          .render("400", { error: { name: error.name, msg: error.message } });
+      }
+    });
+
+        // Load Profiles page
+        app.get("/profile", async (req, res) => {
+          try {
+    
+            res.render("profile");
+          } catch (error) {
+            res
+              .status(400)
+              .render("400", { error: { name: error.name, msg: error.message } });
+          }
+        });
+
+       // Load Billing page
+        app.get("/billing", async (req, res) => {
+          try {
+            
+            res.render("bill");
+          } catch (error) {
+            res
+              .status(400)
+              .render("400", { error: { name: error.name, msg: error.message } });
+          }
+          });
 
   // Load example page and pass in an example by id
   app.get("/example/:id", async (req, res) => {
