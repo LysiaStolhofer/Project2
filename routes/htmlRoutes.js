d2c357bfcdaeed350d6d76305dffc598333815c4
Andrea <andreakwilliams1@gmail.com>
2019-11-23 14:24:36 -0500

    updated routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,58 @@
 var db = require("../models");
 
-module.exports = function (app) {
+module.exports = function(app) {
   // Load index page
   app.get("/", async (req, res) => {
     try {
-      res.render("index", {
+      res.render("index", {});
+    } catch (error) {
+      res
+        .status(400)
+        .render("400", { error: { name: error.name, msg: error.message } });
+    }
+  });
+
+  // Load form page
+  app.get("/enroll", async (req, res) => {
+    try {
+      res.render("newform");
+    } catch (error) {
+      res
+        .status(400)
+        .render("400", { error: { name: error.name, msg: error.message } });
+    }
+  });
+
+  // Load Profiles page
+  app.get("/profile", async (req, res) => {
+    try {
+      res.render("profile");
+    } catch (error) {
+      res
+        .status(400)
+        .render("400", { error: { name: error.name, msg: error.message } });
+    }
+  });
+
+  // Load Billing page
+  app.get("/billing", async (req, res) => {
+    try {
+      res.render("bill");
+    } catch (error) {
+      res
+        .status(400)
+        .render("400", { error: { name: error.name, msg: error.message } });
+    }
+  });
+
+  // Load Profile page and pass in a profile by id
+  app.get("/client/:id", async (req, res) => {
+    try {
+      const profile = await db.Clients.findOne({
+        where: { id: req.params.id }
+      });
+      res.render("singleProfile", {
+        profile: profile
       });
     } catch (error) {
       res
@@ -13,59 +61,8 @@ module.exports = function (app) {
     }
   });
 
-    // Load form page
-  app.get("/enroll", async (req, res) => {
-    try {
-      res.render("newform");
-      catch (error) {
-      res
-        .status(400)
-        .render("400", { error: { name: error.name, msg: error.message } });
-        
-    ;
-  
-        // Load Profiles page
-  app.get("/profile", async (req, res) => {
-    try {
-    
-        res.render("profile");
-      catch (error) {
-      res
-        .status(400)
-        .render("400", { error: { name: error.name, msg: error.message } });
-        
-    ;
-  
-       // Load Billing page
-   app.get("/billing", async (req, res) => {
-    try {
-      
-res.render("bill");
-      catch (error) {
-      res
-        .status(400)
-        .render("400", { error: { name: error.name, msg: error.message } });
-        
-    });
-  
-  // Load Profile page and pass in an profile by id
-  app.get("/client/:id", async (req, res) => {
-    try {
-      const Profile = await db.Clients.findOne({
-        where: { id: req.params.id }
-    });
-    res.render("profile", {
-    example: dbExample
-    ;
-} catch (error) {
-      res
-        .status(400)
-      .render("400", { error: { name: error.name, msg: error.message } });
-    }
-});
-  
-    ender 404 page for any unmatched routes
-      t("*", async (req, res) => {
-    s.render("404");
-    
-      
+  // Render 404 page for any unmatched routes
+  app.get("*", async (req, res) => {
+    res.render("404");
+  });
+};
